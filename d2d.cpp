dab1ed989677dac1879abab83566f02c8328a0bc
shulgaalexey <ShulgaAlexey@gmail.com>
2016-06-30 17:33:58 +0900

    Command autocompletion redesigned with tree model

--- d2d.cpp
+++ d2d.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <map>
 
 #define arr_size(a) (int(sizeof(a)/sizeof(a[0])))
 
@@ -16,135 +17,92 @@ char* my_generator(const char*,int);
 char * dupstr (const char*);
 void *xmalloc (int);
 
-/*const char* cmd [] = {
-	"discovery",
-	"device",
-	"service",
-	"services",
-	"channel",
-	"payload",
-	"connect",
-	"disconnect",
-	"start",
-	"stop",
-	"send",
-	"read",
-	"listen",
-	"unlisten",
-	"quit",
-	" "
-};*/
+//-----------------------------------------------------------------------------
 
-/*
- * DISCOVERY: discovery
- *                      start _timeout_
- *                      stop
- *
- * DEVICE: device _handle_
- *                         services
- *                         id
- *                         name
- *                         type
- *
- * SERVICE: service _handle_
- *                            connect
- *                            disconnect
- *                            start _channel_ _payload_
- *                            stop _channel_ _payload_
- *                            read _channel_ _payload_
- *                            send _channel_ _payload_
- *                            listen
- *                            stoplisten
- *                            id
- *                            version
- */
-const char* d2d_cmd [] = {
-	"discovery",
-	"device",
-	"service",
-	"channel",
-	"payload",
-	"help",
-	"quit",
-	" "
+class cmd_node {
+	public:
+		std::string name;
+		std::map<std::string, cmd_node *> children;
+	public:
+		cmd_node(const std::string &n);
+		~cmd_node();
+	public:
+		void add_child(cmd_node *node);
+		void trace();
 };
 
-const char* discovery_cmd [] = {
-	"start",
-	"stop",
-};
-
-const char* device_cmd [] = {
-	"services",
-	"id",
-	"name",
-	"type"
-};
-
-const char* service_cmd [] = {
-	"connect",
-	"disconnect",
-	"start",
-	"stop",
-	"read",
-	"send",
-	"listen",
-	"stoplisten",
-	"type",
-	"id",
-	"properties"
-};
-
-const char* discovery_timeout_cmd [] = {
-	"0",
-	"15",
-	"30"
-};
-
-
-// This is a fake array, it should be generated dynamically
-const char* device_handle_cmd [] = {
-	"d1",
-	"d2",
-	"d3"
-};
-
-// This is a fake array, it should be generated dynamically
-const char* service_handle_cmd [] = {
-	"s1",
-	"s2",
-	"s3"
-};
-
-// This is a fake array, it should be generated dynamically
-const char* channel_handle_cmd [] = {
-	"c1",
-	"c2",
-	"c3"
-};
-
-// This is a fake array, it should be generated dynamically
-const char* payload_handle_cmd [] = {
-	"p1",
-	"p2",
-	"p3"
-};
-
-
-static const std::string __error_cmd = "ERR";
-
-// Current command
-std::vector<std::string> __command;
-
-
-bool is_cmd(const std::string &word, const char **table, int size) {
-	for(int i = 0; i < size; i ++) {
-		std::string s = table[i];
-		if(word == s)
-			return true;
-	}
-	return false;
+cmd_node::cmd_node(const std::string &n) : name(n) {
 }
+
+cmd_node::~cmd_node() {
+	for(std::map<std::string, cmd_node *>::iterator it = children.begin();
+			it != children.end(); ++it) {
+		cmd_node *node = it->second;
+		delete node;
+	}
+	children.clear();
+}
+
+void cmd_node::add_child(cmd_node *node) {
+	children[node->name] = node;
+}
+
+void cmd_node::trace() {
+
+	printf("{%s}: ", name.c_str());
+	for(std::map<std::string, cmd_node *>::iterator it = children.begin();
+			it != children.end(); ++it) {
+		printf("<%s> ", it->first.c_str());
+	}
+	printf("\n");
+}
+//-----------------------------------------------------------------------------
+
+cmd_node __cmd_root("D2D");
+cmd_node *__selected_cmd = &__cmd_root;
+bool __error_cmd = false;
+
+static void init_cmd_root() {
+	// Discovery
+	__cmd_root.add_child(new cmd_node("discovery"));
+	__cmd_root.children["discovery"]->add_child(new cmd_node("stop"));
+	__cmd_root.children["discovery"]->add_child(new cmd_node("start"));
+	__cmd_root.children["discovery"]->children["start"]->add_child(new cmd_node("0"));
+	__cmd_root.children["discovery"]->children["start"]->add_child(new cmd_node("15"));
+	__cmd_root.children["discovery"]->children["start"]->add_child(new cmd_node("30"));
+
+	// Device
+	__cmd_root.add_child(new cmd_node("device"));
+	__cmd_root.children["device"]->add_child(new cmd_node("dhandle"));
+	__cmd_root.children["device"]->children["dhandle"]->add_child(new cmd_node("services"));
+	__cmd_root.children["device"]->children["dhandle"]->add_child(new cmd_node("id"));
+	__cmd_root.children["device"]->children["dhandle"]->add_child(new cmd_node("name"));
+	__cmd_root.children["device"]->children["dhandle"]->add_child(new cmd_node("type"));
+
+	// Service
+	__cmd_root.add_child(new cmd_node("service"));
+	__cmd_root.children["service"]->add_child(new cmd_node("shandle"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("type"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("id"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("properties"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("connect"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("disconnect"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("start"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("stop"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("read"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("send"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("listen"));
+	__cmd_root.children["service"]->children["shandle"]->add_child(new cmd_node("stoplisten"));
+
+	// Channel, Payload, Help, Quit
+	__cmd_root.add_child(new cmd_node("channel"));
+	__cmd_root.add_child(new cmd_node("payload"));
+	__cmd_root.add_child(new cmd_node("help"));
+	__cmd_root.add_child(new cmd_node("quit"));
+}
+
+//-----------------------------------------------------------------------------
+
 
 static std::string trim(const char *buf) {
 	std::string str(buf);
@@ -160,6 +118,8 @@ int main()
 	printf("\n\n");
 	printf("D2D Convergence Console started\n");
 	printf("enter your commands...\n");
+
+	init_cmd_root();
 
 	rl_attempted_completion_function = my_completion;
 
@@ -180,115 +140,68 @@ int main()
 
 static char** my_completion( const char *text , int start,  int end)
 {
-	//printf(">\n\t\t\t   ...my_completion params: [%s], %d, %d\n", text, start, end);
 	return rl_completion_matches ((char*)text, &my_generator);
 }
 
-void pre_parse_command() {
-	//printf(">preparse: %s\n", rl_line_buffer);
+bool pre_parse_command() {
+
+	// Start command parsing from the beginning of the line
+	__selected_cmd = &__cmd_root;
+
 	std::stringstream ss(rl_line_buffer);
 	std::string word;
-	__command.clear();
+
+	__error_cmd = false;
 	while (ss >> word) {
-		bool is_correct_cmd = false;
-		switch(__command.size()) {
-			case 0:
-				is_correct_cmd = is_cmd(word, d2d_cmd, arr_size(d2d_cmd));
-				break;
-			case 1:
-				//is_correct_cmd = true;
-				is_correct_cmd = is_cmd(word, discovery_cmd, arr_size(discovery_cmd));
-				break;
-			default:
-				is_correct_cmd = true;
-				break;
+		if(__error_cmd) {
+			// Con not continue: somewhere in the middle of the sentence
+			// there is an error of command syntax
+			return false;
 		}
-		if(is_correct_cmd)
-			__command.push_back(word);
-		else
-			__command.push_back(__error_cmd);
 
-	}
-	if((!__command.empty()) && (__command.back() == __error_cmd)) {
-		// We keep adding first word of the command:
-		// should ommit this part
-		__command.pop_back();
-	}
+		// Is this word among children?
+		if(__selected_cmd->children.count(word) > 0) {
 
-	//for(size_t i = 0; i < __command.size(); i ++)
-	//	printf("\n...[%s]\n", __command[i].c_str());
-}
+			// OK: have a part of the command
+			__selected_cmd = __selected_cmd->children[word];
 
-char* my_generator_table(const char* text, const char **table, int size, int *index, const char *tab_name) {
-	//printf(">my_generator_table %s: %s, %d\n", tab_name, text, *index);
-	const int len = strlen (text);
-	while (((*index) < size) && table[*index]) {
-		const char *name = table[*index];
-		(*index)++;
-		if (strncmp (name, text, len) == 0) {
-			//printf(">suggest: %s\n", name);
-			return (dupstr(name));
+		} else {
+			// Something strange is happening
+			// If it is a final word, than the user might keep typing
+			// If it is in the middle of the sentence, than it is an incorrect command
+			__error_cmd = true;
 		}
 	}
-
-	/* If no names matched, then return NULL. */
-	//printf(">my_generator_table: %s, %d HALTED\n", text, *index);
-	return ((char *)NULL);
+	return true;
 }
 
 // http://www.math.utah.edu/docs/info/rlman_2.html#SEC36
 char* my_generator(const char* text, int state)
 {
-	static int list_index = 0;
+	static std::map<std::string, cmd_node *>::iterator it = __selected_cmd->children.begin();
 
 	if (state == 0) { // First attempt to complete current word
-		list_index = 0;
-		pre_parse_command();
-	}
-
-	if (__command.empty()) { // The initial state: no command data entered
-		return my_generator_table(text, d2d_cmd, arr_size(d2d_cmd), &list_index, "d2d_cmd1");
-	} else if(__command.size() == 1) {
-		if(__command[0] == "discovery") {
-			//printf("yooooo\n");
-			return my_generator_table(text, discovery_cmd, arr_size(discovery_cmd), &list_index, "discovery_cmd");
+		if(!pre_parse_command()) {
+			return NULL;
 		}
-		if(__command[0] == "device")
-			return my_generator_table(text, device_cmd, arr_size(device_cmd), &list_index, "device_cmd");
-		if(__command[0] == "service")
-			return my_generator_table(text, service_cmd, arr_size(service_cmd), &list_index, "service_cmd");
-		else
-			return my_generator_table(text, d2d_cmd, arr_size(d2d_cmd), &list_index, "d2d_cmd2");
-	} else if(__command.size() == 2) {
-//for(size_t i = 0; i < __command.size(); i ++)
-//	printf("\n...{%s}\n", __command[i].c_str());
-		return my_generator_table(text, d2d_cmd, arr_size(d2d_cmd), &list_index, "d2d_cmd3");
-	} else {
-//for(size_t i = 0; i < __command.size(); i ++)
-//	printf("\n...{%s}\n", __command[i].c_str());
-		return my_generator_table(text, d2d_cmd, arr_size(d2d_cmd), &list_index, "d2d_cmd4");
+		it = __selected_cmd->children.begin();
 	}
 
-	return NULL;
+	if(__selected_cmd->children.empty()) {
+		// This is the lates component of the command
+		return NULL;
+	}
 
+	const int len = strlen (text);
+	while(it != __selected_cmd->children.end()) {
+		const char *name = it->first.c_str();
+		++it;
 
-	#if 0
-	const char** cmd_array = d2d_cmd;
-	const int n = int(sizeof(d2d_cmd) / sizeof(d2d_cmd[0]));
-	while ((list_index < n) && cmd_array[list_index]) {
-		const char *name = cmd_array[list_index];
-		list_index++;
-
-		if (strncmp (name, text, len) == 0) {
-			__command.push_back((dupstr(name)));
+		if (strncmp (name, text, len) == 0)
 			return (dupstr(name));
-		}
 	}
 
-	/* If no names matched, then return NULL. */
-	return ((char *)NULL);
-#endif
-
+	return ((char *)NULL); // If no names matched, then return NULL.
 }
 
 char * dupstr(const char *s) {
