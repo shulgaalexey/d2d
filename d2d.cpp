3f7f240a0581202724ba7d81a92dc8116640da28
shulgaalexey <ShulgaAlexey@gmail.com>
2016-07-01 14:41:28 +0900

    Added some command to API dispatch

--- d2d.cpp
+++ d2d.cpp
@@ -24,6 +24,8 @@ static void process_discovery(const std::vector<std::string> &cmd);
 static void process_device(const std::vector<std::string> &cmd);
 static void process_service(const std::vector<std::string> &cmd);
 
+static conv_h __convergence_manager = NULL;
+
 
 //-----------------------------------------------------------------------------
 
@@ -130,11 +132,14 @@ static void show_intro() {
 
 	printf("\n");
 
-	const std::string space = "                                           ";
-	const std::string trace = space + "Device-to-Device Convergence Manager Console "
-		"Command Line Interface Application for testing Tiizen D2D Native API ";
+	std::string title = "D2D Convergence CLI 0.0.1 console";
 
-	for(int i = 0; i < int(trace.length()); i ++) {
+	std::string space;
+	for(int i = 0; i < 34; i ++)
+		space += " ";
+	const std::string trace = space + title + " ";
+
+	for(int i = 0; i < int(space.length() + 1); i ++) {
 		printf("\r%s", trace.substr(i, 60).c_str());
 		fflush(stdout);
 		usleep(long_pause);
@@ -144,13 +149,13 @@ static void show_intro() {
 	fflush(stdout);
 	usleep(long_pause);
 
-
-	const std::string title = "D2D Convergence Console\n";
 	for(size_t i = 0; i < title.length(); i ++) {
 		printf("%c", title[i]);
 		fflush(stdout);
 		usleep(quick_pause);
 	}
+
+	printf("\n");
 
 	for(int i = 0; i < 34; i ++) {
 		printf("*");
@@ -174,7 +179,7 @@ static void show_usage() {
 	printf("\n");
 	printf("D2D Convergence CLI 0.0.1\n\n");
 	printf("usage:\n");
-	printf(" help\n");
+	printf(" help                                               - print instructions\n");
 	printf(" discovery start <timeout_seconds>                  - start or stop discovery\n");
 	printf(" device <handle> service | id | name | type         - get device services and properties\n");
 	printf(" service <handle> id | type | properties            - get service properties\n");
@@ -188,18 +193,76 @@ static void show_usage() {
 	printf(" quit\n");
 }
 
+static void show_launch_keys() {
+	printf("\n");
+	printf("D2D Convergence CLI 0.0.1\n\n");
+	printf("usage:\n");
+	printf(" -sdi                                               - skip dramatic intro\n");
+	printf(" -h                                                 - help, print instruction\n");
+	printf("\n");
+}
+
+static void print_conv_error(const int error) {
+	std::string err;
+	switch(error) {
+		case CONV_ERROR_NONE:
+			return; // Not printing success
+		case CONV_ERROR_INVALID_PARAMETER:
+			err = "CONV_ERROR_INVALID_PARAMETER";
+			break;
+		case CONV_ERROR_INVALID_OPERATION:
+			err = "CONV_ERROR_INVALID_OPERATION";
+			break;
+		case CONV_ERROR_OUT_OF_MEMORY:
+			err = "CONV_ERROR_OUT_OF_MEMORY";
+			break;
+		case CONV_ERROR_PERMISSION_DENIED:
+			err = "CONV_ERROR_PERMISSION_DENIED";
+			break;
+		case CONV_ERROR_NOT_SUPPORTED:
+			err = "CONV_ERROR_NOT_SUPPORTED";
+			break;
+		case CONV_ERROR_NO_DATA:
+			err = "CONV_ERROR_NO_DATA";
+			break;
+		default:
+			err = "UNKNOWN ERROR CODE";
+			break;
+
+	}
+	printf("D2D ERROR: %s", err.c_str());
+}
+
+class ConsoleColorSetter {
+	public:
+		ConsoleColorSetter() {
+			printf("%c[%d;%dm", 0x1B, 0, 32);
+		}
+		~ConsoleColorSetter() {
+			printf("%c[%d;%d;%dm", 0x1B, 0, 0, 0);
+		}
+
+};
+
+// TODO disable file browsing for autocompletion
+
 int main(int argc, char *argv[])
 {
+	ConsoleColorSetter ccs;
+
 	// Analyze CLI app arguments
 	bool skip_dramatic_intro = false;
 	bool introduce_usage = false;
 	for(int i = 1; i < argc; i ++) {
 		if(std::string(argv[i]) == "-sdi")
 			skip_dramatic_intro = true;
-		if(std::string(argv[i]) == "-h")
+		else if(std::string(argv[i]) == "-h")
 			introduce_usage = true;
-		else
+		else {
 			printf("Unknown argument [%s]\n", argv[i]);
+			show_launch_keys();
+			return 0;
+		}
 	}
 
 	// Show (or not) the dramatic CLI intro
@@ -214,6 +277,11 @@ int main(int argc, char *argv[])
 
 	// Initialize auto-completion core
 	rl_attempted_completion_function = my_completion;
+
+	// Create convergence manager
+	int error = conv_create(&__convergence_manager);
+	print_conv_error(error);
+
 
 	// Continuously process user input
 	char *buf = NULL;
@@ -244,11 +312,6 @@ int main(int argc, char *argv[])
 			continue;
 		}
 
-		/*printf("  D2D << ");
-		for(size_t i = 0; i < cmd.size(); i ++)
-			printf("{%s} ", cmd[i].c_str());
-		printf("\n");*/
-
 		const std::string cmd_type = cmd[0];
 
 		// TODO: link functions to the nodes of behavioural graph
@@ -269,21 +332,150 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	// Destroy convergence manager
+	error = conv_destroy(__convergence_manager);
+	print_conv_error(error);
+
 	return 0;
 }
 
-static void process_discovery(const std::vector<std::string> &cmd) {
-	printf("  BOOOM: process_discovery >> ");
-	for(size_t i = 0; i < cmd.size(); i ++)
-		printf("{%s} ", cmd[i].c_str());
-	printf("\n");
+static void __conv_discovery_cb(conv_device_h device_handle,
+		conv_discovery_result_e result, void* user_data) {
 }
 
+// commands:
+// discovery start 15
+// discovery stop
+static void process_discovery(const std::vector<std::string> &cmd) {
+	/*printf("  BOOOM: process_discovery >> ");
+	for(size_t i = 0; i < cmd.size(); i ++)
+		printf("{%s} ", cmd[i].c_str());
+	printf("\n");*/
+
+	if(cmd.size() < 1) {
+		printf("Incorrect format of discovery command: no start or stop instruction\n");
+		return;
+	}
+
+	if(cmd[1] == "start") {
+		int timeout_seconds = 0; // default value
+
+		if(cmd.size() > 2)
+			timeout_seconds = atoi(cmd[2].c_str());
+
+		const int error = conv_discovery_start(__convergence_manager,
+				timeout_seconds, __conv_discovery_cb, NULL);
+		print_conv_error(error);
+	} else if(cmd[1] == "stop") {
+		const int error = conv_discovery_stop(__convergence_manager);
+		print_conv_error(error);
+	} else {
+		printf("Incorrect instruction of discovery\n");
+	}
+}
+
+static void __conv_service_foreach_cb(conv_service_h service_handle,
+		void* user_data) {
+	if(!service_handle)
+		return;
+	printf("handle: 0x%p", (void *)service_handle);
+
+	{ // Printing service ID
+		char *id = NULL;
+		int error = conv_service_get_property_string(service_handle,
+				CONV_SERVICE_ID, &id);
+		print_conv_error(error);
+		if(error == CONV_ERROR_NONE) {
+			printf("\tid: %s", id);
+			free(id);
+		} else {
+			printf("\tid: NULL");
+		}
+	}
+
+	{ // Printing service version
+		char *version = NULL;
+		int error = conv_service_get_property_string(service_handle,
+				CONV_SERVICE_VERSION, &version);
+		print_conv_error(error);
+		if(error == CONV_ERROR_NONE) {
+			printf("\tversion: %s", version);
+			free(version);
+		} else {
+			printf("\tversion: NULL");
+		}
+	}
+}
+
+conv_device_h get_device_handle_by_handle_string(const std::string &handle_str) {
+	return NULL;
+}
+
+conv_device_h get_device_handle_by_name(const std::string &name) {
+	return NULL;
+}
+
+// commands:
+// device <dhandle1 | TizenA> services
+// device <dhandle1 | TizenA> id
+// device <dhandle1 | TizenA> name
+// device <dhandle1 | TizenA> type
 static void process_device(const std::vector<std::string> &cmd) {
 	printf("  BOOOM: process_device >> ");
 	for(size_t i = 0; i < cmd.size(); i ++)
 		printf("{%s} ", cmd[i].c_str());
 	printf("\n");
+
+
+	if(cmd.size() != 3) {
+		printf("Incorrect format of discovery command: no start or stop instruction\n");
+		return;
+	}
+
+	// Assuming the device is specified with handle
+	conv_device_h device = get_device_handle_by_handle_string(cmd[1]);
+	if(!device)
+		// Assuming the device is specified with name
+		device = get_device_handle_by_name(cmd[1]);
+
+	if(!device) {
+		printf("Incorrect handle or name of the device\n");
+		return; // No device handle, can not continue
+	}
+
+	if(cmd[2] == "services") {
+		const int error = conv_device_foreach_service(device,
+				__conv_service_foreach_cb, NULL);
+		print_conv_error(error);
+	} else if(cmd[2] == "id") {
+		char *id = NULL;
+		const int error = conv_device_get_property_string(device,
+				CONV_DEVICE_ID, &id);
+		print_conv_error(error);
+		if(error == CONV_ERROR_NONE) {
+			printf("%s\n", id);
+			free(id);
+		}
+	} else if(cmd[2] == "name") {
+		char *name = NULL;
+		const int error = conv_device_get_property_string(device,
+				CONV_DEVICE_NAME, &name);
+		print_conv_error(error);
+		if(error == CONV_ERROR_NONE) {
+			printf("%s\n", name);
+			free(name);
+		}
+	} else if(cmd[2] == "type") {
+		char *type = NULL;
+		const int error = conv_device_get_property_string(device,
+				CONV_DEVICE_TYPE, &type);
+		print_conv_error(error);
+		if(error == CONV_ERROR_NONE) {
+			printf("%s\n", type);
+			free(type);
+		}
+	}
+
 }
 
 static void process_service(const std::vector<std::string> &cmd) {
