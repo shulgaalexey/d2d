9bf2d2fd97bd41bcf7717134003137ad5aeda1d1
shulgaalexey <shulgaalexey@gmail.com>
2016-11-20 16:33:59 +0900

    Initial commit for voice control feature

---
+++ console/src/d2d_voice_control.cpp
@@ -0,0 +1,8 @@
+#include "d2d_voice_control.h"
+
+
+d2d_voice_control::d2d_voice_control() {
+}
+
+d2d_voice_control::~d2d_voice_control() {
+}
---
+++ console/src/d2d_voice_control.h
@@ -0,0 +1,13 @@
+#ifndef __D2D_CONV_VOICE_CONTROL_H__
+#define __D2D_CONV_VOICE_CONTROL_H__
+
+class d2d_voice_control {
+	public:
+		d2d_voice_control();
+		~d2d_voice_control();
+};
+
+
+#endif /*__D2D_CONV_VOICE_CONTROL_H__*/
+
+
