404b420488372908677249af3c5b95697a7503ec
shulgaalexey <shulgaalexey@gmail.com>
2016-11-20 23:31:43 +0900

    Added general voice proccessing function

--- console/src/d2d_voice_control.cpp
+++ console/src/d2d_voice_control.cpp
@@ -1,8 +1,25 @@
 #include "d2d_voice_control.h"
-
+using namespace std;
 
 d2d_voice_control::d2d_voice_control() {
 }
 
 d2d_voice_control::~d2d_voice_control() {
+}
+
+
+std::vector<std::string> d2d_voice_control::process_mic() {
+	// 1. Get signal from mic
+
+	// 2. Send it to the voice server
+
+	// 3. Receive recognized text
+	vector<string> cmd;
+
+	// just for test
+	cmd.push_back("discovery");
+	cmd.push_back("start");
+	cmd.push_back("60");
+
+	return cmd;
 }
--- console/src/d2d_voice_control.h
+++ console/src/d2d_voice_control.h
@@ -1,10 +1,15 @@
 #ifndef __D2D_CONV_VOICE_CONTROL_H__
 #define __D2D_CONV_VOICE_CONTROL_H__
 
+#include <string>
+#include <vector>
+
 class d2d_voice_control {
 	public:
 		d2d_voice_control();
 		~d2d_voice_control();
+	public:
+		std::vector<std::string> process_mic();
 };
 
 
